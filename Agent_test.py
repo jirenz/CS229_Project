1fb554806ba90938ebcff7c0260f3261cbb1d5fd
Hubert Teo <hubert.teo.hk@gmail.com>
2016-05-22 15:40:08 -0700

    qlearning

--- Agent_test.py
+++ Agent_test.py
@@ -12,38 +12,46 @@ import shelve
 
 from projectfiles.deck_loader import DeckLoader
 from projectfiles.hearthlogger import Hearthlogger
+from projectfiles.agent import *
+from projectfiles.feature_extract import *
 
-def test_agent_once():
-    generator = RandomDeckGenerator()
-    deck1 = generator.generate()
-    deck2 = deck1.copy()
-    game = Game([deck1, deck2], [TradeAgent(), RandomAgent()])
-    new_game = game.copy()
-    try:
-        new_game.start_with_debug()
-    except Exception as e:
-       #print(json.dumps(new_game.__to_json__(), default=lambda o: o.__to_json__(), indent=1))
-       #print(new_game._all_cards_played)
-       del new_game
-       del game_log
-       return False
-    print("winning agent: " + new_game.winner.agent.__class__.__name__)
-    return new_game.winner.agent.__class__.__name__
+def test_agent_once(ql):
+	generator = RandomDeckGenerator()
+	deck1 = generator.generate()
+	deck2 = deck1.copy()
 
-def test_agent(number):
-    i = 0
-    winning_count = {}
-    while i < number:
-        winner = test_agent_once()
-        if winner:
-            i += 1
-            if winner in winning_count:
-                winning_count[winner] += 1
-            else:
-                winning_count[winner] = 1
-    print(winning_count)
+	game = Game([deck1, deck2], [ql, TradeAgent()])
+	# game = Game([deck1, deck2], [TradeAgent(), RandomAgent()])
+	new_game = game.copy()
+	try:
+		new_game.start_with_debug()
+		# new_game.start()
+	except Exception as e:
+		#print(json.dumps(new_game.__to_json__(), default=lambda o: o.__to_json__(), indent=1))
+		#print(new_game._all_cards_played)
+		del new_game
+		return False
+	print("winning agent: " + new_game.winner.agent.__class__.__name__)
+	# print(ql.weights)
+	return new_game.winner.agent.__class__.__name__
+
+def run_agent(ql, number):
+	i = 0
+	winning_count = {}
+	while i < number:
+		winner = test_agent_once(ql)
+		if winner:
+			i += 1
+			if winner in winning_count:
+				winning_count[winner] += 1
+			else:
+				winning_count[winner] = 1
+	print(winning_count)
 
 if __name__ == "__main__":
-    test_agent(int(sys.argv[1]))
+	ql = AIAgent(eta = 0.05, explore_prob = 0.1, feature_extractor = feature_extractor)
+	run_agent(ql, int(sys.argv[1]))
+	ql.explore_prob = 0.0
+	run_agent(ql, int(sys.argv[2]))
 
 
