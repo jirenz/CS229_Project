f841988f282fd1f4dc8c01755fe54be0908e9c8a
Hubert Teo <hubert.teo.hk@gmail.com>
2016-05-22 14:49:46 -0700

    stuff

--- projectfiles/agent.py
+++ projectfiles/agent.py
@@ -1,95 +1,87 @@
-from hearthbreaker.agents import *
+from hearthbreaker.agents.basic_agents import *
 import collections
+import numpy as np
 
 class DoFixedThingsMachine(Agent):
-    def __init__(self, choose_index, entity_index, target_index, minion_position_index = 0):
-        self.choose_index = choose_index
-        self.entity_index = entity_index
-        self.target_index = target_index
-        self.minion_position_index = minion_position_index
+	def __init__(self, chosen_index, entity_index, target_index, minion_position_index = 0):
+		self.chosen_index = chosen_index
+		self.entity_index = entity_index
+		self.target_index = target_index
+		self.minion_position_index = minion_position_index
 
-    def do_turn(self, player):
-		if choose_index == 0:
-            self.player.minions[entity_index].attack()
-        elif choose_index == 1:
-            self.player.hero.attack()
-        elif choose_index == 2:
-            self.player.game.play_card(self.player.hand[entity_index])
-        elif choose_index == 3:
-            self.player.hero.power.use()
+	def do_card_check(self, cards):
+		return [True, True, True, True]
 
-    def choose_target(self, targets):
+	def do_turn(self, player):
+		if self.chosen_index == 0:
+			player.minions[self.entity_index].attack()
+		elif self.chosen_index == 1:
+			player.hero.attack()
+		elif self.chosen_index == 2:
+			player.game.play_card(player.hand[self.entity_index])
+		elif self.chosen_index == 3:
+			player.hero.power.use()
+		elif self.chosen_index == 4:
+			pass
+
+	def choose_target(self, targets):
 		if self.target_index is not None:
 			return targets[self.target_index]
-		else
-			return random.randint(0, len(targets) - 1)
+		else:
+			return targets[random.randint(0, len(targets) - 1)]
 
-    def choose_index(self, card, player):
-        return minion_position_index
-        
-    def choose_option(self, options, player):
-        return options[random.randint(0, len(options) - 1)]
+	def choose_index(self, card, player):
+		return self.minion_position_index
+
+	def choose_option(self, options, player):
+		return options[random.randint(0, len(options) - 1)]
 
 class AIAgent(DoNothingAgent):
 	def __init__(self, eta, explore_prob, feature_extractor):
-        super().__init__()
+		super().__init__()
 		self.eta = eta
 		self.explore_prob = explore_prob
 		self.feature_extractor = feature_extractor
-		self.weights = collection.defaultdict(float)
+		self.weights = None
 
-    def do_card_check(self, cards):
-        return [True, True, True, True]
+	def do_card_check(self, cards):
+		return [True, True, True, True]
 
-    def do_turn(self, player):
-        attack_minions = [minion for minion in filter(lambda minion: minion.can_attack(), player.minions)]
-        if player.hero.can_attack():
-            attack_minions.append(player.hero)
-        playable_cards = [card for card in filter(lambda card: card.can_use(player, player.game), player.hand)]
-        if player.hero.power.can_use():
-            possible_actions = len(attack_minions) + len(playable_cards) + 1
-        else:
-            possible_actions = len(attack_minions) + len(playable_cards)
-        if possible_actions > 0:
-            action = random.randint(0, possible_actions - 1)
-            if player.hero.power.can_use() and action == possible_actions - 1:
-                player.hero.power.use()
-            elif action < len(attack_minions):
-                attack_minions[action].attack()
-            else:
-                player.game.play_card(playable_cards[action - len(attack_minions)])
-            return True
-        else:
-            return False
-	
 	def get_enemy_targets(self, player):
-        found_taunt = False
-        targets = []
-        for enemy in player.game.other_player.minions:
-            if enemy.taunt and enemy.can_be_attacked():
-                found_taunt = True
-            if enemy.can_be_attacked():
-                targets.append(enemy)
+		found_taunt = False
+		targets = []
+		for enemy in player.game.other_player.minions:
+			if enemy.taunt and enemy.can_be_attacked():
+				found_taunt = True
+			if enemy.can_be_attacked():
+				targets.append(enemy)
 
-        if found_taunt:
-            targets = [target for target in targets if target.taunt]
-        else:
-            targets.append(self.player.game.other_player.hero)
+		if found_taunt:
+			targets = [target for target in targets if target.taunt]
+		else:
+			targets.append(self.player.game.other_player.hero)
 		return targets
 
 	def get_actions(self, player):
-		if player.game.game_ended(): return []
+		if player.game.game_ended: return []
 
 		actions = []
 		enemy_targets = self.get_enemy_targets(player)
-		for i, attack_minion in filter(lambda (i, minion): minion.can_attack(), enumerate(player.minions)):
-			actions += [(0, i, target) for target in xrange(len(enemy_targets))]
+		for i, attack_minion in filter(lambda p: p[1].can_attack(), enumerate(player.minions)):
+			actions += [(0, i, target) for target in range(len(enemy_targets))]
 		if player.hero.can_attack():
-			actions += [(1, None, target) for target in xrange(len(minion_targets))]
-		for i, card in filter(lambda (i, card): card.can_use(player, player.game), enumerate(player.hand)):
-			actions += [(2, i, target) for target in xrange(len(card.targets))]
+			actions += [(1, None, target) for target in range(len(minion_targets))]
+		for i, card in filter(lambda p: p[1].can_use(player, player.game), enumerate(player.hand)):
+			try:
+				actions += [(2, i, target) for target in range(len(card.targets))]
+			except:
+				actions += [(2, i, None)]
 		if player.hero.power.can_use():
 			actions += [(3, None, None)]
+
+		if len(actions) == 0:
+			actions += [(4, None, None)]
+
 		return actions
 
 	def decide(self, actions):
@@ -97,30 +89,33 @@ class AIAgent(DoNothingAgent):
 			return random.choice(actions)
 		else:
 			return max((self.Q(self.player.game, action), action) for action in actions)[1]
-	
+
 	def Q(self, game, action):
 		copied_game = game.copy()
 		copied_game.current_player.agent = DoFixedThingsMachine(*action)
-		copied_game.current_player.agent.do_turn()
-		total = 0.0
-		for f, v in self.feature_extractor(copied_game.current_player):
-			total += self.weights[f] * v
+		copied_game.current_player.agent.do_turn(copied_game.current_player)
+		total = np.dot(self.weights, self.feature_extractor(copied_game.current_player))
+		del copied_game
 		return total
 
-    def do_turn(self, player):
-        # copied_game = self.player.game.copy()
+	def do_turn(self, player):
+		# copied_game = self.player.game.copy()
+		if self.weights is None:
+			self.weights = np.zeros(np.size(self.feature_extractor(player)))
+
+		self.player = player
 		actions = self.get_actions(self.player)
 		if len(actions) == 0:
 			return
 
-        best_decision = self.decide(actions) #copied_game)
+		best_decision = self.decide(actions) #copied_game)
 
 		oldQ = self.Q(self.player.game, best_decision)
-        self.machine = DoFixedThingsMachine(*best_decision)
-        self.machine.do_turn()
-		
+		self.machine = DoFixedThingsMachine(*best_decision)
+		self.machine.do_turn(self.player)
+
 		reward = 0.0
-		if self.player.game.game_ended():
+		if self.player.game.game_ended:
 			if self.player.game.winner is self.player:
 				newV = 100.0
 			elif self.player.game.winner is None:
@@ -128,17 +123,17 @@ class AIAgent(DoNothingAgent):
 			else:
 				newV = -100.0
 		else:
-			newV = max(self.Q(self.player.game, action) for action in self.get_actions(self.player))
+			actions = self.get_actions(self.player)
+			newV = max(self.Q(self.player.game, action) for action in actions)
 
-		for f, v in self.feature_extractor(self.player):
-			self.weights += self.eta * (reward + 1.0 * newV - oldQ)
+		self.weights += self.eta * (reward + 1.0 * newV - oldQ) * self.feature_extractor(self.player)
 
-    def choose_target(self, targets):
-        return self.machine.choose_target(targets)
+	def choose_target(self, targets):
+		return self.machine.choose_target(targets)
 
-    def choose_index(self, card, player):
-        return self.machine.choose_index(card, player)
+	def choose_index(self, card, player):
+		return self.machine.choose_index(card, player)
 
-    def choose_option(self, options, player):
-        options = self.filter_options(options, player)
-        return self.machine.choose_option(options, player)
+	def choose_option(self, options, player):
+		options = self.filter_options(options, player)
+		return self.machine.choose_option(options, player)
--- projectfiles/feature_extract.py
+++ projectfiles/feature_extract.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def feature_extractor(player):
 	oppo = player.opponent
 	feat = []
@@ -6,35 +8,36 @@ def feature_extractor(player):
 	count = 0
 	tmp = []
 	for i in player.minions:
-		tmp.append([i.attack, i.health, 1])
+		tmp.append([i.calculate_attack(), i.health, 1])
 		count += 1
 	tmp.sort()
 	for i in tmp: feat += i
 	for i in range(0, 7 - count): feat += [0, 0, 0]
-	feat += [player.health, player.armor]
+	feat += [player.hero.health, player.hero.armor]
 	
 	# add opponent's information
 	count = 0
 	tmp = []
 	for i in oppo.minions:
-		tmp.append([i.attack, i.health, 1])
+		tmp.append([i.calculate_attack(), i.health, 1])
 		count += 1
 	tmp.sort()
 	for i in tmp: feat += i
 	for i in range(0, 7 - count): feat += [0, 0, 0]
-	feat += [oppo.health, oppo.armor]
+	# feat += [oppo.hero.health, oppo.hero.armor]
 
 	# add other information
 	feat += [player.mana, oppo.mana]
 	feat += [player.current_overload, oppo.current_overload]
 	feat += [player.upcoming_overload, oppo.upcoming_overload]
 	feat += [player.max_mana, oppo.max_mana]
-	feat += [len(player.deck), len(oppo.deck)]
+	feat += [len(player.deck.cards), len(oppo.deck.cards)]
 	feat += [player.fatigue, oppo.fatigue]
 	feat += [player.spell_damage, oppo.spell_damage]
 	feat += [player.spell_multiplier, oppo.spell_multiplier]
 	feat += [player.heal_multiplier, oppo.heal_multiplier]
 	feat += [player.heal_does_damage, oppo.heal_does_damage]
 	feat += [player.cards_played, oppo.cards_played]
-	
-	return feat
+
+	# print(feat)
+	return np.array(feat, dtype=np.float64)
