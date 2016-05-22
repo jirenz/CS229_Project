60306848919d5d0c98b7a292ad4ad49dec845437
jirenz <zhujiren1155@hotmail.com>
2016-05-22 14:36:33 -0700

    extractor

--- projectfiles/feature_extract.py
+++ projectfiles/feature_extract.py
@@ -23,5 +23,18 @@ def feature_extractor(player):
 	for i in tmp: feat += i
 	for i in range(0, 7 - count): feat += [0, 0, 0]
 	feat += [oppo.health, oppo.armor]
+
+	# add other information
+	feat += [player.mana, oppo.mana]
+	feat += [player.current_overload, oppo.current_overload]
+	feat += [player.upcoming_overload, oppo.upcoming_overload]
+	feat += [player.max_mana, oppo.max_mana]
+	feat += [len(player.deck), len(oppo.deck)]
+	feat += [player.fatigue, oppo.fatigue]
+	feat += [player.spell_damage, oppo.spell_damage]
+	feat += [player.spell_multiplier, oppo.spell_multiplier]
+	feat += [player.heal_multiplier, oppo.heal_multiplier]
+	feat += [player.heal_does_damage, oppo.heal_does_damage]
+	feat += [player.cards_played, oppo.cards_played]
 	
 	return feat
