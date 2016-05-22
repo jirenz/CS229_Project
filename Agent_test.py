1743a711a1e3e9f75e8aebdf25123f780b3fb1e5
jirenz <zhujiren1155@hotmail.com>
2016-05-22 14:25:08 -0700

    logger

---
+++ Agent_test.py
@@ -0,0 +1,49 @@
+from hearthbreaker.agents import registry
+from hearthbreaker.cards.heroes import hero_for_class
+from hearthbreaker.constants import CHARACTER_CLASS
+from hearthbreaker.engine import Deck, card_lookup, Game
+from hearthbreaker.cards import *
+from hearthbreaker.replay import *
+from hearthbreaker.agents import *
+
+from projectfiles.random_deck_generator import RandomDeckGenerator
+import sys
+import shelve
+
+from projectfiles.deck_loader import DeckLoader
+from projectfiles.hearthlogger import Hearthlogger
+
+def test_agent_once():
+    generator = RandomDeckGenerator()
+    deck1 = generator.generate()
+    deck2 = deck1.copy()
+    game = Game([deck1, deck2], [TradeAgent(), RandomAgent()])
+    new_game = game.copy()
+    try:
+        new_game.start()
+    except Exception as e:
+       #print(json.dumps(new_game.__to_json__(), default=lambda o: o.__to_json__(), indent=1))
+       #print(new_game._all_cards_played)
+       del new_game
+       del game_log
+       return False
+    print("winning agent: " + new_game.winner.agent.__class__.__name__)
+    return new_game.winner.agent.__class__.__name__
+
+def test_agent(number):
+    i = 0
+    winning_count = {}
+    while i < number:
+        winner = test_agent_once()
+        if winner:
+            i += 1
+            if winner in winning_count:
+                winning_count[winner] += 1
+            else:
+                winning_count[winner] = 1
+    print(winning_count)
+
+if __name__ == "__main__":
+    test_agent(int(sys.argv[1]))
+
+
--- boardstate_test.py
+++ boardstate_test.py
@@ -11,41 +11,7 @@ import sys
 import shelve
 
 from projectfiles.deck_loader import DeckLoader
-
-class Hearthlogger:
-
-    def __init__(self):
-        self.game = None
-        self.states = []
-        self.winner = None
-        self.games = []
-
-    def attach(self, game):
-        game.tracking = True
-        game.logger = self
-        self.game = game
-
-    def log(self, json_str):
-        self.states.append(json_str)
-        self.games.append(self.game.copy())
-
-    def save(self, file_name = "default_log"):
-        file_name += ".hslog"
-        with open(file_name, "w") as text_file:
-            if self.winner:
-                text_file.write("winner: " + self.winner + '\n')
-            for state in self.states:
-                text_file.write(state)
-                text_file.write('\n')
-        text_file.close()
-        print("Saved to " + file_name + '.\n')
-
-    def shelf(self,number):
-        s = shelve.open("projectfiles/LR-statevalue/gamefiles.dat",writeback=True)
-        if not "gamelogger" in s:
-            s["gamelogger"] = []
-        s["gamelogger"].append(self)
-        s.close()
+from projectfiles.hearthlogger import Hearthlogger
 
 def generate_one(filename):
     loader = DeckLoader()
