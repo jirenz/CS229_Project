135219959e03d33ae2477f45dbac8d18ffdf3631
jirenz <zhujiren1155@hotmail.com>
2016-06-02 01:47:10 -0700

    merge

--- Plotting.m
+++ Plotting.m
@@ -15,3 +15,13 @@ plot(YL, 'color', [0, 1, 0], 'LineWidth',3)
 set(gca, 'XTick', 1:length(X))
 set(gca,'XTickLabel', X)
 %
+xlabel('Training set data size')
+ylabel('Winning rate')
+rez=1200; %resolution (dpi) of final graphic
+f=gcf; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+resolution=get(0,'ScreenPixelsPerInch'); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+path=''; %the folder where you want to put the file
+name='Learning_curve_supervised.png'; %what you want the file to be called
+print(f,fullfile(path,name),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
