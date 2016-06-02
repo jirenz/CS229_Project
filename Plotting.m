f38c85da665e74c9144dfeb19f553618b5f7fb93
jirenz <zhujiren1155@hotmail.com>
2016-06-02 01:59:50 -0700

    merge

--- Plotting.m
+++ Plotting.m
@@ -6,11 +6,29 @@ clear
 X = [10, 20, 50, 200, 500, 1000, 2000, 4000, 12000, 20000, 40000]
 YD = [2, 50, 67, 81, 75, 71, 78, 73, 74, 75, 76]
 YL = [1, 5, 9, 59, 52, 62, 78, 69, 81, 76, 77]
-%XQ = [500]
+1, 5, 10, 20, 50 
+20, 100, 200, 400, 1000
+XQ = [2,3.4,4,4.8,6]
+XQ1 = [41, 60, 64, 65, 40]
+XQ2 = [52, 49, 50, 59, 50]
 %YQ = [0, 62, 50, 0, 55]
+%models/ql_sp_relative_50.t trade 100 2 => 50
+%models/ql_sp_relative_20.t trade 100 2 => 62
+%models/ql_sp_relative_1.t trade 100 2 => 52
+%models/ql_sp_relative_5.t trade 100 2 => 49
+%models/ql_sp_relative_10.t trade 100 2 => 50
+%models/ql_sp_relative_20.t trade 100 2 => 55
+%models/ql_fs_resource_1.t trade 100 2 => 41
+%models/ql_fs_resource_5.t trade 100 2 => 60
+%models/ql_fs_resource_10.t trade 100 2 => 64
+%models/ql_fs_resource_20.t trade 100 2 => 65
+%models/ql_fs_resource_50_2.t trade 100 2 => 40
+
 hold on
 plot(YD, 'color', [1, 0, 0], 'LineWidth',3)
 plot(YL, 'color', [0, 1, 0], 'LineWidth',3)
+plot(XQ, XQ1, 'color', [1, 0, 1], 'LineWidth',3)
+plot(XQ, XQ2, 'color', [0, 1, 1], 'LineWidth',3)
 %plot(YQ, 'color', [0, 0, 1])
 set(gca, 'XTick', 1:length(X))
 set(gca,'XTickLabel', X)
